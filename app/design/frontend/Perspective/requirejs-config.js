47fb3036d22061a4cef3f669a00d13f1320d6345
Victoria Sira <victoria.s@overdose.digital>
2026-06-16 15:10:32 +0300

    Task9.3.STD-907 A message has been added to the console when selecting a shipping method

--- app/design/frontend/Perspective/requirejs-config.js
+++ app/design/frontend/Perspective/requirejs-config.js
@@ -1,7 +1,8 @@
 let config = {
     map: {
         '*': {
-            'Magento_Swatches/js/swatch-renderer': 'Magento_Swatches/js/swatch-renderer'
+            'Magento_Swatches/js/swatch-renderer': 'Magento_Swatches/js/swatch-renderer',
+            'Magento_Checkout/js/view/shipping': 'Magento_Checkout/js/view/shipping'
         }
     }
 };
